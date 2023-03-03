9bcafc1a7e6716894da9088d35b7081fd2bde661
widmaker666 <alexandre.hontcharouk69@gmail.com>
2023-03-03 17:23:12 +0100

    fin de page random

--- src/components/PaintRandom.jsx
+++ src/components/PaintRandom.jsx
@@ -3,29 +3,41 @@ import axios from "axios";
 
 const PaintRandom = () => {
   const [paintData, setPaintData] = useState([]);
-  const [searchBar, setSearchBar] = useState("");
+  const [description, setDescription] = useState({});
   const index = Math.floor(Math.random() * 99);
   /* const randomIndex = paintData[index] */
-  console.log(paintData);
+  const apiId = paintData.id;
   //-Nouvelle API Chicago museum plus simple je pense à tester
   const API = "https://api.artic.edu/api/v1/artworks?limit=100";
 
   //-Ici on a l'API qui explique la peinture via son id : https://api.artic.edu/api/v1/artworks/{id}/manifest.json
 
-  //-Nouvelle variable API pour travailler sur la barre de recherche mais il faut afficher le résultat dans une page où l'on montre qu'une peinture
-  //!En utilisant le .id on à la possibilité de récuperer les informations propre à la peinture du peintre tapé et donc image_id.
-  const apiSearch =
-    "https://api.artic.edu/api/v1/artworks/search?q=" + searchBar;
-  /* console.log(apiSearch); */
+  const apiDescription = `https://api.artic.edu/api/v1/artworks/${apiId}/manifest.json`;
 
   //-UseEffect utilisé lorsque axios à besoin de faire jouer sa recherche
   useEffect(() => {
     axios.get(API).then((res) => setPaintData(res.data.data[index]));
   }, []);
 
+  useEffect(() => {
+    axios
+      .get(apiDescription)
+      .then((res) => res.data)
+      .then((data) => setDescription(data.description[0]));
+  }, [paintData]);
+
   return (
     <div className="paints">
-      <h1>{paintData.title}</h1>
+      <img
+        src={`https://www.artic.edu/iiif/2/${paintData.image_id}/full/843,/0/default.jpg`}
+        alt={paintData.title}
+      />
+      <div className="infos-card">
+        <h2>{paintData.title}</h2>
+        <h4>{paintData.artist_title}</h4>
+        <p>{paintData.place_of_origin}</p>
+        <p>{description.value}</p>
+      </div>
     </div>
   );
 };
@@ -34,5 +46,3 @@ export default PaintRandom;
 
 //!Il faut que je récupère l'id grâce à la premiere API, pour pouvoir utiliser le lien d'API de description pour afficher la description de la peinture avec : .value
 //!Ensuite créer un nouveau const [valueId, setValueId] = useState("") pour afficher la description de la peinture avec la nouvelle API
-
-
